85c3c4ae245bcc50a519b83ec0c7c121534faa3c
NorbertNagy1982 <nagynorbert1@gmail.com>
2024-01-09 07:44:41 +0100

    new location

---
+++ educationsystem/src/main/java/educationsystem/example/educationsystem/EducationsystemApplication.java
@@ -0,0 +1,13 @@
+package educationsystem.example.educationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationsystemApplication.class, args);
+	}
+
+}
---
+++ educationsystem/src/test/java/educationsystem/example/educationsystem/EducationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package educationsystem.example.educationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
